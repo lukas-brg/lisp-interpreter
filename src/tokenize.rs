f611a9951c9f1cbc8e233ee26a7b589e104a1de1
lukas-brg <gh.lukas.brg@gmail.com>
2024-12-18 23:04:56 +0100

    tokenizing numbers

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -1,6 +1,7 @@
 
 use crate::token::{Token, TokenContent};
 use crate::token::TokenContext;
+use crate::token::TokenType::NUMBER;
 
 fn parse_number(input: &str, start: usize) -> (usize, TokenContent) {
 
@@ -8,10 +9,12 @@ fn parse_number(input: &str, start: usize) -> (usize, TokenContent) {
 
     let mut num_str = String::new();
 
-    let mut c = input.as_bytes()[0] as char;
+    let mut c = input.as_bytes()[start] as char;
     num_str.push(c);
     let mut float = false;
+    c = input.as_bytes()[index] as char;
     while index < input.len() && (c.is_numeric() || c == '.') {
+        c = input.as_bytes()[index] as char;
         if c == '.'{
             float = true;
         }
@@ -21,10 +24,11 @@ fn parse_number(input: &str, start: usize) -> (usize, TokenContent) {
     let content;
 
     if float {
-        let parsed_float: f64 = num_str.parse().expect("Failed to parse float");
+        let parsed_float: f64 = num_str.trim().parse().expect("Failed to parse float");
         content = TokenContent::Float(parsed_float);
     } else {
-        let parsed_int: i32 = num_str.parse().expect("Failed to parse int");
+        println!(" {}", num_str);
+        let parsed_int: i32 = num_str.trim().parse::<i32>().unwrap();
         content = TokenContent::Int(parsed_int);
     }
 
@@ -42,7 +46,12 @@ pub fn tokenize_line(input: &str, tokens: &mut Vec<Token>, line_num: usize) {
 
         let c = input.as_bytes()[index] as char; // Get the character at the current index
 
-        if c.is_numeric() {}
+        if c.is_numeric() {
+            let (i, content) = parse_number(input, index);
+            index = i;
+            tokens.push(Token::new(NUMBER, context, Some(content)));
+            continue;
+        }
 
 
         let token = match c {
@@ -55,8 +64,8 @@ pub fn tokenize_line(input: &str, tokens: &mut Vec<Token>, line_num: usize) {
             _ => {}
         };
 
+        index += 1;
     }
-    index += 1;
 }
 
 pub fn tokenize(input: &str) -> Vec<Token> {
@@ -66,8 +75,9 @@ pub fn tokenize(input: &str) -> Vec<Token> {
 
         println!("Line {}: {}", line_number + 1, line);
         tokenize_line(line, &mut tokens, line_number)
-
     }
+
+    println!("Tokens: {:?}", tokens);
     return tokens;
 
 }
