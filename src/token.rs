e269b208c61d33930b0bca98a4bf2c3d7680a82f
lukas-brg <gh.lukas.brg@gmail.com>
2024-12-19 07:39:38 +0100

    cleanup

--- src/token.rs
+++ src/token.rs
@@ -10,22 +10,19 @@ pub enum TokenType {
     IDENTIFIER,
 }
 
-// Define the TokenContext struct
 #[derive(Debug, Clone)]
 pub struct TokenContext {
     pub line_number: usize,
     pub column_number: usize,
 }
 
-// Define the Token struct
 #[derive(Debug, Clone)]
 pub struct Token {
     pub token_type: TokenType,
     pub context: TokenContext,
-    pub content: Option<TokenContent>, // This is an Option for content
+    pub content: Option<TokenContent>, 
 }
 
-// Define an enum for the content inside a Token
 #[derive(Debug, Clone)]
 pub enum TokenContent {
     Float(f64),
